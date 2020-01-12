99d65601fcec1d69a0abd7ff3e53345cd6450fa0
Shreyas <dlshreyas30@gmail.com>
2020-01-13 00:37:06 +0530

    Stability improvement using Hooks

---
+++ src/hooks/useResults.js
@@ -0,0 +1,35 @@
+import { useEffect , useState } from 'react'
+import foursquare from '../api/foursquare'
+
+export default () => {
+  const [results , setResults] = useState([])
+  const [errorMessage , setErrorMessage] = useState('')
+
+  const searchApi = async (initialTerm) => {
+      try {
+              const responseFromApi = await foursquare.get('/search',{
+                  params : {
+                    ll : '40.7,-74',
+                  client_id : 'UMKW4DAWHH3JNEKJK30GSFVXV1I4NEXMTWJXX2S1HJAMNKJI',
+                  client_secret : 'FH1GPRDAHI025P1R203M0C30TKOVC1HUJCYB3MIKNV0F0P3X',
+                  v : 20200112,
+                  query : initialTerm
+                }
+              })
+              setResults(responseFromApi.data.response.venues)
+          } catch (err) {
+              setErrorMessage('Something went wrong')
+              console.log(err)
+          }
+  }
+
+  // CALL searchApi when Component first rendered
+  // searchApi('coffee')  BAD CODE !!!!
+
+  useEffect(() => {
+    searchApi('coffee')
+  }, [])
+
+  return ( [ searchApi , results , errorMessage])
+
+}
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import foursquare from '../api/foursquare'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
   const [searchTerm , setSearchTerm] = useState('')
-  const [results , setResults] = useState([])
-  const [errorMessage , setErrorMessage] = useState('')
-
-  const searchApi = async (initialTerm) => {
-      try {
-              const responseFromApi = await foursquare.get('/search',{
-                  params : {
-                    ll : '40.7,-74',
-                  client_id : 'UMKW4DAWHH3JNEKJK30GSFVXV1I4NEXMTWJXX2S1HJAMNKJI',
-                  client_secret : 'FH1GPRDAHI025P1R203M0C30TKOVC1HUJCYB3MIKNV0F0P3X',
-                  v : 20200112,
-                  query : initialTerm
-                }
-              })
-              setResults(responseFromApi.data.response.venues)
-          } catch (err) {
-              setErrorMessage('Something went wrong')
-              console.log(err)
-          }
-  }
-
-  // CALL searchApi when Component first rendered
-  // searchApi('coffee')  BAD CODE !!!! 
+  const [searchApi , results , errorMessage] = useResults()
 
   return (
       <View >
